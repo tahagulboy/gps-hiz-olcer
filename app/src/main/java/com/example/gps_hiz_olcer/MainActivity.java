8757a45bef7a3ae0b5f4949850a549203467a33c
Taha Gülboy <tahagulboi@gmail.com>
2024-05-07 00:45:29 +0300

    update

--- app/src/main/java/com/example/gps_hiz_olcer/MainActivity.java
+++ app/src/main/java/com/example/gps_hiz_olcer/MainActivity.java
@@ -9,7 +9,6 @@ import android.location.LocationManager;
 import android.os.Bundle;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
@@ -18,7 +17,6 @@ public class MainActivity extends AppCompatActivity {
 
     private static final int REQUEST_LOCATION_PERMISSION = 1;
     private TextView textSpeed;
-    private Location previousLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +25,6 @@ public class MainActivity extends AppCompatActivity {
 
         textSpeed = findViewById(R.id.text_speed);
 
-        // GPS izni alınması
         if (ActivityCompat.checkSelfPermission(this,
                 Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this,
@@ -44,7 +41,7 @@ public class MainActivity extends AppCompatActivity {
             if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 startLocationUpdates();
             } else {
-                Toast.makeText(this, "Permission Denied!", Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "İzin Reddedildi!", Toast.LENGTH_SHORT).show();
             }
         }
     }
@@ -52,39 +49,22 @@ public class MainActivity extends AppCompatActivity {
     private void startLocationUpdates() {
         LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
         if (locationManager != null) {
-            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                // TODO: Consider calling
-                //    ActivityCompat#requestPermissions
-                // here to request the missing permissions, and then overriding
-                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                //                                          int[] grantResults)
-                // to handle the case where the user grants the permission. See the documentation
-                // for ActivityCompat#requestPermissions for more details.
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                    ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
                 return;
             }
+
             locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 0, new LocationListener() {
                 @SuppressLint({"SetTextI18n", "DefaultLocale"})
                 @Override
                 public void onLocationChanged(@NonNull Location location) {
-                    if (previousLocation == null) {
-                        previousLocation = location;
-                        return;
-                    }
-
-                    // Hız hesaplaması
-                    float distanceInMeters = previousLocation.distanceTo(location);
-                    float timeInSeconds = (location.getTime() - previousLocation.getTime()) / 1000f; // milliseconds to seconds
-                    float speedInMetersPerSecond = distanceInMeters / timeInSeconds;
-                    float speedInKmPerHour = speedInMetersPerSecond * 3.6f; // Meters per second to km/h
-                    textSpeed.setText("Hız: " + String.format("%.2f", speedInKmPerHour) + " km/s");
-
-                    previousLocation = location;
+                    int speed = (int)((location.getSpeed()*3600)/1000);
+                    textSpeed.setText("Hız: " + speed + " km/s");
                 }
 
                 @Override
                 public void onStatusChanged(String provider, int status, Bundle extras) {
                 }
-                
 
                 @Override
                 public void onProviderEnabled(@NonNull String provider) {
